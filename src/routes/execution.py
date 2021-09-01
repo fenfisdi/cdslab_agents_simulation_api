151be3bd7d20864031221679cf431d7f8f418857
David Peña <dpenah@intergrupo.com>
2021-09-01 15:12:19 -0500

    Feature: Create execution route

--- src/routes/execution.py
+++ src/routes/execution.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from starlette.status import HTTP_200_OK
+from starlette.status import HTTP_200_OK, HTTP_500_INTERNAL_SERVER_ERROR
 
+from src.use_case import FinishEmergencyExecution
 from src.utils.messages import SimulationMessage
 from src.utils.response import UJSONResponse
 
@@ -8,5 +9,13 @@ execution_routes = APIRouter()
 
 
 @execution_routes.post("/execute")
-def execute_simulation():
-    return UJSONResponse(SimulationMessage.success, HTTP_200_OK)
+def execute_simulation(configuration: dict):
+    simulation_uuid = configuration.get("configuration").get("identifier")
+    try:
+        return UJSONResponse(SimulationMessage.success, HTTP_200_OK)
+    except Exception as error:
+        FinishEmergencyExecution.handle(simulation_uuid)
+        return UJSONResponse(
+            str(error),
+            HTTP_500_INTERNAL_SERVER_ERROR
+        )
