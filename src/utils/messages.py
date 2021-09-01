bae76520d402826a125dc6520243d02270cea645
David Peña <dpenah@intergrupo.com>
2021-09-01 15:12:43 -0500

    Feature: Create cloud api execution

--- src/utils/messages.py
+++ src/utils/messages.py
@@ -4,3 +4,4 @@ from dataclasses import dataclass
 @dataclass
 class SimulationMessage:
     success: str = 'Simulation has been executed'
+    internal_error: str = 'Simulation has invalid codification'
