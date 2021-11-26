3819a99ffcb6a6efde140ddeaca9c0f7511ad9fe
David Peña <dpenah@intergrupo.com>
2021-11-26 12:03:14 -0500

    Feature: Update execution object

--- src/routes/execution.py
+++ src/routes/execution.py
@@ -2,9 +2,7 @@ from fastapi import APIRouter
 from starlette.background import BackgroundTasks
 from starlette.status import HTTP_200_OK
 
-from src.use_case import (
-    StartExecution
-)
+from src.use_case import StartExecution
 from src.utils.messages import SimulationMessage
 from src.utils.response import UJSONResponse
 
@@ -12,7 +10,10 @@ execution_routes = APIRouter()
 
 
 @execution_routes.post("/execute")
-def execute_simulation(data: dict, background_tasks: BackgroundTasks):
+def execute_simulation(
+    data: dict,
+    background_tasks: BackgroundTasks
+):
     background_tasks.add_task(StartExecution.handle, data)
 
     return UJSONResponse(SimulationMessage.success, HTTP_200_OK)
